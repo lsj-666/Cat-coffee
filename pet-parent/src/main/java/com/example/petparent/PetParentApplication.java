fe1eee78c631edb73f1095761a71ff8d4ea6ba3f
luoshijie <1225988200@qq.com>
2023-12-29 23:07:31 +0800

    fix：前端代码

--- pet-parent/src/main/java/com/example/petparent/PetParentApplication.java
+++ pet-parent/src/main/java/com/example/petparent/PetParentApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @SpringBootApplication
 public class PetParentApplication {
